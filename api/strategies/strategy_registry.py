29241c1186e60855277e20703d2136a8c9a77c40
ideasdeapps-collab <ideasdeapps@gmail.com>
2026-08-19 22:25:11 -0600

    feat: register complete trading strategy catalog

--- api/strategies/strategy_registry.py
+++ api/strategies/strategy_registry.py
@@ -1,7 +1,27 @@
 from .momentum_breakout import MomentumBreakout
+from .ema_pullback import EMAPullback
+from .vwap_reclaim import VWAPReclaim
+from .mean_reversion import MeanReversion
+from .rsi_divergence import RSIDivergence
+from .macd_trend import MACDTrend
+from .bollinger_squeeze import BollingerSqueeze
+from .opening_range_breakout import OpeningRangeBreakout
+from .volume_breakout import VolumeBreakout
+from .trend_following import TrendFollowing
+from .liquidity_sweep import LiquiditySweep
 
 STRATEGIES = {
-    "Momentum Breakout": MomentumBreakout()
+    "Momentum Breakout": MomentumBreakout(),
+    "EMA Pullback": EMAPullback(),
+    "VWAP Reclaim": VWAPReclaim(),
+    "Mean Reversion": MeanReversion(),
+    "RSI Divergence": RSIDivergence(),
+    "MACD Trend": MACDTrend(),
+    "Bollinger Squeeze": BollingerSqueeze(),
+    "Opening Range Breakout": OpeningRangeBreakout(),
+    "Volume Breakout": VolumeBreakout(),
+    "Trend Following": TrendFollowing(),
+    "Liquidity Sweep": LiquiditySweep(),
 }
 
 
